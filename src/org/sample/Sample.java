13fd77f0ea0f0bca1def989e8ef65860ffe06f2e
Kamalb319 <kamalbashavpr@gmail.com>
2025-06-29 12:51:40 +0530

    anu aaded few codes

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,15 +1,24 @@
 package org.sample;
 
 public class Sample {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		System.out.println(a);
+
+		String s = "Kamalbasha@123";
+
+		boolean s1 = s.startsWith("K");
+		System.out.println(s1);
+		
+		
+		
+	Boolean s2 =	s.isEmpty();
 	
-	
-public static void main(String[] args) {
-	
-	int  a = 10;
-	
-	System.out.println(a);
-	
-	
-}
+	System.out.println(s2);
+
+	}
 
 }
